fcf2e64e4500c029a8fd4cbc6c73507de309e7b3
Ronit <ronit2001krish@gmail.com>
2024-07-07 22:54:19 +0530

    react hooks by webdev simplified

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+function getno() {
+  console.log('ji');
+  return 4;
+}
 
 function App() {
+  const [state, setState] = useState(() => {
+    return { count: 4, theme: 'black' };
+  });
+
+  function decrementCount() {
+    setState(prevState => {
+      // return { count: prevCount.count - 1 }; // incorrect 
+      return { ...prevState, count: prevState.count - 1 }; // correct 
+    });
+  }
+
+  function incrementCount() {
+    setState(prevState => {
+      return { ...prevState, count: prevState.count + 1 };
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={decrementCount}>-</button>
+      <span>{state.count}</span>
+      <span>{state.theme}</span>
+      <button onClick={incrementCount}>+</button>
+    </>
   );
 }
 
